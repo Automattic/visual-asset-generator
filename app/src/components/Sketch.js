ab06e01cc3f4b5ed60900af22328b1d3e6086111
Jeff Ong <jeff.ong@automattic.com>
2019-01-30 14:55:27 -0500

    figured out drawing location, but image resampling is terrible in p5

--- app/src/components/Sketch.js
+++ app/src/components/Sketch.js
@@ -44,20 +44,35 @@ function drawFrame(p, frame, frameMask){
   frame.updatePixels();
   frame.mask(frameMask);
   p.tint(255, 225);
-  p.image(frame, p.width / 2, p.height / 2);
+  p.image(frame, 0, 0);
   p.noTint();
 }
 
-function drawPortrait(p, portrait, portraitMask, relativeBox){
+
+function drawPortrait(p, portrait, portraitMask, r){
   p.background(255);
-  p.imageMode(p.CENTER);
-  portrait.resize(0, p.height);
-  portrait.mask(portraitMask); // create the fade effect
-  if (relativeBox !== undefined){
-    console.log(relativeBox._x, relativeBox._y);
-    p.image(portrait, SPOTLIGHT.x + (relativeBox._x - 0.5) * portrait.width, SPOTLIGHT.y + (relativeBox._y * portrait.height / 2));
+  p.imageMode(p.CORNER);
+  // portrait.mask(portraitMask); // create the fade effect
+
+  if (r !== undefined){
+    portrait.resize(0, p.height);
+    const scalar = SPOTLIGHT.diameter / r.box._width;
+    portrait.resize(portrait.width * scalar, 0);
+
+    console.log(r.relativeBox);
+
+    let shift = (r.relativeBox._x + r.relativeBox._width) * portrait.width;
+    let portrait_x = SPOTLIGHT.x - shift;
+    p.image(portrait, portrait_x, SPOTLIGHT.y - (r.relativeBox._height * portrait.height));
+
+    // shift = (1 - r.relativeBox._x) * portrait.width;
+    // portrait_x = SPOTLIGHT.x - shift;
+    // p.image(portrait, portrait_x, SPOTLIGHT.y - (r.relativeBox._height * portrait.height));
+    // p.image(portrait, 0, 0);
   } else {
-    p.image(portrait, SPOTLIGHT.x, SPOTLIGHT.y);
+    // Set height of the portrait to the height of the canvas
+    portrait.resize(0, p.height);
+    p.image(portrait, 0, 0);
   }
 }
 
@@ -73,7 +88,11 @@ function sketch(p){
   let img, imgMask, frame, frameMask;
 
 	p.preload = function() {
-		img = p.loadImage('assets/removebg_2.png');
+		// img = p.loadImage('assets/GettyImages-97559100.jpg');
+		// img = p.loadImage('assets/removebg_2.png');
+		// img = p.loadImage('assets/GettyImages-908040972.jpg');
+		// img = p.loadImage('assets/GettyImages-915095050.jpg');
+		img = p.loadImage('assets/GettyImages-1012208544.jpg');
 		imgMask = p.loadImage('assets/mask_reverse.png');
     frameMask = p.loadImage('assets/mask_16-9.png');
 	}
@@ -83,13 +102,22 @@ function sketch(p){
 
     drawPortrait(p, img, imgMask);
 
-    drawSpotlight(p);
+    // drawSpotlight(p);
+
+    const scaleFactors = [2, 1.5, 1, 0.75, .5];
+    // const horizontalShift = [ -0.5, 0, 0.5 ];
 
     detectFaces().then((r)=>{
-      // result = r.relativeBox;
-      drawPortrait(p, img, imgMask, r.relativeBox);
+      drawPortrait(p, img, imgMask, r);
       drawFrame(p, frame, frameMask);
-      drawSpotlight(p);
+      // p.save(`shift.png`);
+
+      // for (let i = 0; i < scaleFactors.length; i++){
+      //     drawPortrait(p, img, imgMask, r, scaleFactors[i]);
+      //     drawFrame(p, frame, frameMask);
+      //     // drawSpotlight(p);
+      //     p.save(`${i}.png`);
+      // }
     });
 	}
 
