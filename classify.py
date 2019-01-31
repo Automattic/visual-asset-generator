42c3864832c5e120b3beadb68601705881618219
Jeff Ong <jeff.ong@automattic.com>
2019-01-31 18:48:04 -0500

    consistently finding faces

--- classify.py
+++ classify.py
@@ -9,7 +9,7 @@ DIMENSIONS = {
 
 SPOTLIGHT = {
   'x': round(DIMENSIONS['width'] * 0.6),
-  'y': round(DIMENSIONS['height'] * 1 / 3),
+  'y': round(DIMENSIONS['height'] * 0.3),
   'd': round(DIMENSIONS['width'] * 0.375)
 }
 
@@ -27,7 +27,15 @@ class Spotlight:
         img = cv2.imread(self.img['path'])
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-        (x,y,w,h) = faces[0]
+        for f in faces:
+            (x,y,w,h) = f
+            if w > 300:
+                break
+
+        if w < 300:
+            print('no faces found')
+            sys.exit()
+
         self.face['x'] = x
         self.face['y'] = y
         self.face['w'] = w
@@ -50,7 +58,7 @@ class Spotlight:
         self.db.blendMode('normal')
         self.db.image(frame, (0,0), .9)
 
-    def renderPortrait(self):
+    def renderPortrait(self, magic):
         self.db.fill(1,1,1,1)
         self.db.rect(0,0,self.width, self.height)
 
@@ -58,7 +66,10 @@ class Spotlight:
         im.photoEffectMono()
 
         #scale the portrait to fit the spotlight
-        sf = self.spotlight['d'] / self.face['w']
+        print(self.face)
+        print(self.img)
+        print(self.db.imageSize(im))
+        sf = self.spotlight['d'] / (self.face['w'] / self.img['w'] * self.db.imageSize(im)[0]) * magic 
         print(sf)
         im.lanczosScaleTransform(sf)
         print(self.db.imageSize(im))
@@ -70,10 +81,10 @@ class Spotlight:
         self.db.image(im, (im_x, im_y))
         self.db.oval(self.spotlight['x'], self.spotlight['y'], 10, 10)
 
-    def render(self):
+    def render(self, magic):
         self.db.newDrawing()
         self.db.size(self.width, self.height)
-        self.renderPortrait()
+        self.renderPortrait(magic)
         self.renderFrame()
 
     def save(self, fp):
@@ -84,10 +95,15 @@ class Spotlight:
 
 if __name__ == "__main__":
     RECOLETA = drawBot.installFont('/Users/jeff-ong/Library/Fonts/Latinotype - Recoleta Regular.otf')
-    ad = Spotlight(drawBot, 'assets/GettyImages-908040972.jpg', DIMENSIONS, SPOTLIGHT)
+    # ad = Spotlight(drawBot, 'assets/GettyImages-915095050.jpg', DIMENSIONS, SPOTLIGHT) # silver fox 
+    # ad = Spotlight(drawBot, 'assets/GettyImages-1020581962.jpg', DIMENSIONS, SPOTLIGHT) # josh musick
+    # ad = Spotlight(drawBot, 'assets/GettyImages-908040972.jpg', DIMENSIONS, SPOTLIGHT) # glasses woman
+    ad = Spotlight(drawBot, 'assets/GettyImages-1012208544.jpg', DIMENSIONS, SPOTLIGHT) # glasses woman
     ad.classify()
-    ad.render()
-    ad.save("outputs/class_test.png")
+    magic_numbers = [1, 4/5, 3/5, 2/3, 1/2]
+    for magic_number in magic_numbers:
+        ad.render(magic_number)
+        ad.save("outputs/scale_test_{}.png".format(magic_number))
     ad.end()
 
 
