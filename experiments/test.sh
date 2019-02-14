e21abcddaa9fc249da96813123dd3e2b11167858
Jeff Ong <jeff.ong@automattic.com>
2019-02-14 18:20:21 -0500

    menu edits

---
+++ experiments/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Bash Menu Script Example
+
+PS3='Please enter your choice: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
+            for o in "${!options[@]}"; do echo "$((o+1)))" "${options[$o]}"; done
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 # Bash Menu Script Example
 
 source cv-py3/bin/activate
-echo "Hello!"
 PS3='What size asset do you want? (1/2/3/4): '
 options=("300x250" "300x600" "970x250" "Quit")
 select opt in "${options[@]}"
@@ -12,17 +11,19 @@ do
       read -p "What do you want it to say? (40 character limit): " fullname
       echo "Generating assets..."
       python3 app.py --format 300_250 --copy "$fullname"
-      open outputs/renders
+      for o in "${!options[@]}"; do echo "$((o+1)))" "${options[$o]}"; done
       ;;
     "300x600")
+      read -p "What do you want it to say? (40 character limit): " fullname
       echo "Generating assets..."
       python3 app.py --format 300_600 --copy "$fullname"
-      open outputs/renders
+      for o in "${!options[@]}"; do echo "$((o+1)))" "${options[$o]}"; done
       ;;
     "970x250")
+      read -p "What do you want it to say? (40 character limit): " fullname
       echo "Generating assets..."
       python3 app.py --format 970_250 --copy "$fullname"
-      open outputs/renders
+      for o in "${!options[@]}"; do echo "$((o+1)))" "${options[$o]}"; done
       ;;
     "Quit")
       break
