cc96221b33fc37bd9c440f9bd610a3fbffe04892
Jeff Ong <jeff.ong@automattic.com>
2019-02-22 14:12:10 -0500

    move react p5 app to archive

--- experiments/svg.py
+++ experiments/svg.py
@@ -3,20 +3,23 @@ sys.path.append('./')
 sys.path.append('./../')
 import drawBot as db
 
-width = 600
-height = 500
+width = 300
+height = 250
 db.newDrawing()
 db.size(width, height)
 # db.fill(0, .9)
 # db.rect(0, 0, width, height)
 # db.stroke(1)
 frame = db.ImageObject('assets/300_250_a@8x.png')
+sf = width / frame.size()[0]
+db.scale(sf)
 db.image(frame, (0,0))
+db.scale(1/sf)
 badge = db.ImageObject('assets/badge.png')
-print(badge.size())
-sf = 1000 / db.imageSize(badge)[0]
+sf = 500 / db.imageSize(badge)[0]
 badge.lanczosScaleTransform(sf)
-db.blendMode('normal')
+print(badge.size())
+db.blendMode('difference')
 db.image(badge, (30, 30))
 db.saveImage('outputs/svg_test.png')
 db.endDrawing()
